25c150a471a45ef50b942f86d565ca620054254b
Manoj <manojkumarbonala3@gmail.com>
2024-05-03 09:18:14 +0530

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
 import Hero from "@/components/Hero";
 import { Services } from "@/components/Services";
+import { CardContainer } from "@/components/ui/3d-card";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center pt-36 px-8">
       <Hero />
-      <div className="pt-56">
+      <CardContainer className="pt-56">
         <Image
           src="/img.png"
           width={1000}
           height={500}
           alt="Picture of the author"
         />
-      </div>
+      </CardContainer>
       <div className=" pt-72 text-center space-y-3">
         <h1 className="text-6xl font-semibold  bg-gradient-to-r from-violet-500 to-fuchsia-500 inline-block text-transparent bg-clip-text">
           Our Services
