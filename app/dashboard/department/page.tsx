cf7d40c47b3b59da1431ea53b99a4d780cdaad88
Manoj <manojkumarbonala3@gmail.com>
2024-05-05 11:17:44 +0530

    code

--- app/dashboard/department/page.tsx
+++ app/dashboard/department/page.tsx
@@ -3,6 +3,8 @@ import PageTitle from "@/components/PageTitle";
 import { SetStateAction, useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Curriculum } from "./_components/curriculum";
+import Events from "./_components/events";
+import Complaints from "./_components/complaints";
 
 
 
