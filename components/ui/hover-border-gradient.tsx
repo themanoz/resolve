111c5585f1deb42c90a5833d595195e8eb74b286
Manoj Kumar <manojkumarbonala3@gmail.com>
2024-05-06 09:47:49 +0530

    Update hover-border-gradient.tsx

--- components/ui/hover-border-gradient.tsx
+++ components/ui/hover-border-gradient.tsx
@@ -54,7 +54,7 @@ export function HoverBorderGradient({
       }, duration * 1000);
       return () => clearInterval(interval);
     }
-  }, [hovered],duration);
+  }, [hovered,duration]);
   return (
     <Tag
       onMouseEnter={(event: React.MouseEvent<HTMLDivElement>) => {
