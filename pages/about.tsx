7b2cd6ba3de0662f6541bc7556f9e8091b84dbc6
Manoj Kumar <manojkumarbonala3@gmail.com>
2024-05-06 09:52:28 +0530

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // import { cn } from "@/app/utils/cn";
 // import { Spotlight } from "@/components/ui/Spotlight";
 
-export function About() {
+export default function About() {
   return (
     <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       {/* <Spotlight
