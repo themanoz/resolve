708889de7c9265d40d5cf98057e6061f85ce1464
Manoj <manojkumarbonala3@gmail.com>
2024-05-07 17:05:49 +0530

    code

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -15,60 +15,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Card } from "@/components/ui/card";
 
-const years = [
-  { label: "P1", value: "p1" },
-  { label: "P2", value: "p2" },
-  { label: "E1", value: "e1" },
-  { label: "E2", value: "e2" },
-  { label: "E3", value: "e3" },
-  { label: "E4", value: "e4" },
-] as const;
-
-const branches = [
-  { label: "Computer Science", value: "com" },
-  { label: "Electrical", value: "ele" },
-  { label: "Chemical", value: "che" },
-  { label: "Metallurgy", value: "met" },
-  { label: "Mechanical", value: "mec" },
-  { label: "Civil", value: "civ" },
-] as const;
-
-const scholarships = [
-  { label: "Eligible", value: "eli" },
-  { label: "Not-eligible", value: "not" },
-];
-
 const formSchema = z.object({
-  firstName: z.string().min(2, {
-    message: "Enter your first name.",
-  }),
-  lastName: z.string().min(2, {
-    message: "Enter your last name.",
-  }),
   email: z.string().email({
     message: "Enter a valid email address.",
   }),
-  phoneNumber: z.string().min(10, {
-    message: "Enter a valid phone number.",
-  }),
   password: z.string().min(8, {
     message: "Enter your password",
-  }),
-  confirmPassword: z.string().min(8, {
-    message: "Re-enter your password",
-  }),
-  gender: z.enum(["male", "female", "other"], {
-    required_error: "You need to select a gender.",
-  }),
-  year: z.string({
-    required_error: "Please select a year.",
-  }),
-  branch: z.string({
-    required_error: "Please select a branch.",
-  }),
-  profileImage: z.union([z.string(), z.instanceof(Buffer)]).optional(),
-  scholarships: z.string({
-    required_error: "Please select your scholarship eligibility",
   }),
 });
 
@@ -92,7 +44,7 @@ export default function Signin() {
       <Card className="py-8 px-6 space-y-6 w-fit">
         <div className="text-center">
           <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-            Login
+            Signin
           </h2>
           <p className="text-neutral-600 text-sm max-w-sm  dark:text-neutral-300">
             Enter your credentials to login into your account
@@ -130,12 +82,11 @@ export default function Signin() {
                 </FormItem>
               )}
             />
+            <Button className="w-full">Signin</Button>
           </form>
-
-          <Button className="w-full">Signin</Button>
-            <p className="text-center">
-              Don&apos;t have an account? <Link href="/auth/signup">Signup</Link>
-            </p>
+          <p className="text-center">
+            Don&apos;t have an account? <Link href="/auth/signup">Signup</Link>
+          </p>
         </Form>
       </Card>
     </section>
